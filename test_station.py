6a234623d7628e5f31597ebf8c55f570aadea994
AmyNaden <aln33@cam.ac.uk>
2022-02-06 23:29:27 +0000

    added back test station checks

--- test_station.py
+++ test_station.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 """Unit test for the station module"""
 
-from floodsystem.station import MonitoringStation
+from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations 
 
 
 def test_create_monitoring_station():
@@ -25,3 +25,24 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
+
+def test_monitoring_station_method():
+    # Creating three stations known to be consistent or inconsistent
+    inconsistent = MonitoringStation(0, 0, 0, 0, (3.6, 2.5), 0, 0)
+    no_range = MonitoringStation(0, 0, 0, 0, None, 0, 0)
+    consistent = MonitoringStation(0, 0, 0, 0, (1.5, 3.4), 0, 0)
+
+    # Asserting what is known to be true
+    assert MonitoringStation.typical_range_consistent(inconsistent) == False
+    assert MonitoringStation.typical_range_consistent(no_range) == False
+    assert MonitoringStation.typical_range_consistent(consistent) == True
+
+def test_inconsistent_typical_range_stations():
+    # Creating three stations known to be consistent or inconsistent 
+    inconsistent = MonitoringStation(0, 0, 0, 0, (3.6, 2.5), 0, 0)
+    no_range = MonitoringStation(0, 0, 0, 0, None, 0, 0)
+    consistent = MonitoringStation(0, 0, 0, 0, (1.5, 3.4), 0, 0)
+    stations = [inconsistent, no_range, consistent]
+    
+    # Asserting what is known to be true
+    assert inconsistent_typical_range_stations(stations) == [inconsistent.name, no_range.name]
