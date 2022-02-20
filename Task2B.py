a1bfb98633e96d70c5311c4aa4d6ef8c77bb4cae
AmyNaden <aln33@cam.ac.uk>
2022-02-20 21:20:36 +0000

    Corrected flood module test

--- Task2B.py
+++ Task2B.py
@@ -1,6 +1,7 @@
 from floodsystem.flood import stations_level_over_threshold
 from floodsystem.stationdata import build_station_list
 from floodsystem.stationdata import update_water_levels
+from floodsystem.station import MonitoringStation
 
 
 def run():
--- test_flood.py
+++ test_flood.py
@@ -1,4 +1,3 @@
-'''
 """Unit test for the flood module"""
 from floodsystem.station import MonitoringStation
 from floodsystem.flood import stations_level_over_threshold
@@ -11,7 +10,7 @@ def test_stations_level_over_threshold():
     station2.latest_level = 0.5
     # Add two stations to a list
     stations = [station1, station2]
-    # Call function passing list and threshold value 0.8
-    over_thresh = stations_level_over_threshold(stations, 0.8)
-    assert over_thresh == [station1, 2.7]
-'''
+    # Call function passing list and threshold value 0.16
+    over_thresh = stations_level_over_threshold(stations, 0.16)
+    assert over_thresh == [station1, 0.18181818181818196]
+
