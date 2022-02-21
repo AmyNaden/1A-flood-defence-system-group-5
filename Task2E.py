aec324956841f0870c9f4072cc263160cf91bdf4
AmyNaden <aln33@cam.ac.uk>
2022-02-21 09:06:17 +0000

    Task2E with general plot attempt 1

--- Task2E.py
+++ Task2E.py
@@ -3,11 +3,11 @@
 import datetime
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level
-from floodsystem.plot import plot_water_levels
+from floodsystem.plot import plot_water_levels, plot_water_levels_general
 from floodsystem.datafetcher import fetch_measure_levels
 
 
-def run():
+def run1():
     # Build list of stations
     stations = build_station_list()
 
@@ -15,15 +15,37 @@ def run():
     update_water_levels(stations)
 
     # Get list of station objects with top 5 relative water level
-    top10 = stations_highest_rel_level(stations, 5)
+    top5 = stations_highest_rel_level(stations, 5)
 
     for i in range(5):
-        station = top10[i][0]
+        station = top5[i][0]
         dt = 10
         dates, levels = fetch_measure_levels(station.measure_id,
                                      dt=datetime.timedelta(days=dt))
         plot_water_levels(station, dates, levels)
 
+def run2():
+    '''Extension - Test plot_water_level_general'''
+    # Build list of stations
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    # Get list of station objects with top 5 relative water level
+    top5 = stations_highest_rel_level(stations, 5)
+
+    # Create list of station objects
+    stations = []
+    for i in range(5):
+        stations.append(top5[i][0])
+    
+    plot_water_levels_general(stations)
+
+
+
 if __name__ == "__main__":
     print("\n*** Task 2C: CUED Part IA Flood Warning System ***")
-    run()
+    run1()
+    print("\n*** Task 2C Extension: CUED Part IA Flood Warning System ***")
+    run2()
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from floodsystem.datafetcher import fetch_measure_levels
+import datetime
 
 def plot_water_levels(station, dates, levels):
     '''Required for task 2E - displays a plot of water level data against time for a station'''
@@ -11,6 +13,30 @@ def plot_water_levels(station, dates, levels):
     plt.ylabel('water level (m)')
     plt.xticks(rotation=45);
     plt.title(station.name)
+
+    # Display plot
+    plt.tight_layout()  # This makes sure plot does not cut off date labels
+
+    plt.show()
+
+def plot_water_levels_general(stations):
+    '''Extension task - Generalise plot to take a list of up to 6 stations and display the level at each station as single plot'''
+    station_names = []
+    # Loop through list of stations
+    for i in range(len(stations)):
+        station_names.append(stations[i].name)
+        dt = 10
+        dates, levels = fetch_measure_levels(stations[i].measure_id,
+                                     dt=datetime.timedelta(days=dt))
+        # Plot
+        plt.plot(dates, levels)
+        # Include plot lines for the typical low and high levels
+    
+    # Axes should be labelled and use the station name as the plot title
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45);
+    plt.title(station_names)
 
     # Display plot
     plt.tight_layout()  # This makes sure plot does not cut off date labels
