c7effca79bfe25905b804fc81486737e4cab14ff
db876 <db876@cam.ac.uk>
2022-02-22 19:37:32 +0000

    2F Working

--- Task2F.py
+++ Task2F.py
@@ -4,12 +4,20 @@ from floodsystem.flood import stations_highest_rel_level
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.plot import plot_water_level_with_fit
 
-stations = build_station_list()
+def run():
+    # Fetching list of stations and updating water levels
+    stations = build_station_list()
+    update_water_levels(stations)
+    # Getting stations with 5 highest relative water levels
+    top5 = stations_highest_rel_level(stations, 5)
+    # Iterating through these 5 stations
+    for i in range(len(top5)):
+        # Fetching the data for various times in the past 2 days
+       dates = fetch_measure_levels(top5[i][0].measure_id, dt=datetime.timedelta(days=2))[0]
+       levels = fetch_measure_levels(top5[i][0].measure_id, dt=datetime.timedelta(days=2))[1]
+       # Plotting a graph using this data
+       plot_water_level_with_fit(top5[i][0], dates, levels, 4)
 
-update_water_levels(stations)
-
-top5 = stations_highest_rel_level(stations, 5)
-
-for i in range(len(top5)):
-   dates, levels = fetch_measure_levels(top5[i].measure_id, dt=datetime.timedelta(days=2))
-   plot_water_level_with_fit(top5[i].name, dates, levels, 4)
+if __name__ == "__main__":
+    print("*** Task 2F: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -6,10 +6,10 @@ def polyfit(dates, levels, p):
     # Looping through dates to assign dates to x and water level to y
     dates = datex.date2num(dates)
     # Finds coefficients of best fit polynomial
-    p_coeff = np.polyfit(dates, levels, p)
+    p_coeff = np.polyfit(dates - dates[-1], levels, p)
     # Converting coefficient into a polynomial that can be evaluated
     poly = np.poly1d(p_coeff)
     # Finding date offset in days
-    date_now = datetime.today
-    d0 = datex.date2num(date_now) - dates[0]
+    date_now = datetime.date.today()
+    d0 = datex.date2num(date_now) - dates[-1]
     return (poly, d0)
